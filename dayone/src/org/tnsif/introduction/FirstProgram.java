c645fac880a3d4a9932d9f666b2386bdc710cee0
dell <dell@DESKTOP-AKG5SKU>
2023-08-09 01:11:34 +0530

    first commit

---
+++ dayone/src/org/tnsif/introduction/FirstProgram.java
@@ -0,0 +1,14 @@
+package org.tnsif.introduction;
+//program to demonstrate on simple java program
+//single line comment
+/*-------
+ * 
+ */
+public class FirstProgram {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+		System.out.println("welcome");
+	}
+
+}
